1d59a9354849a5317ddde257d19b59f5ce89df04
Agung Nugraha <agungvr@gmail.com>
2023-03-21 18:08:47 +0700

    add demo.js

---
+++ public/demo.js
@@ -0,0 +1,4 @@
+(function () {
+  document.getElementById("root").innerHTML =
+    '<div style="height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center;"><h1>Demo source-map vulnerability</h1></div>';
+})();
